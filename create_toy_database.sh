91388d787aaa35bae8b6366a95d9ab422d587948
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2021-05-05 17:23:12 +0530

    Redirected some output to /dev/null

--- create_toy_database.sh
+++ create_toy_database.sh
@@ -2,6 +2,8 @@ cd install/bin
 ./initdb -D data
 ./pg_ctl -D data -l logfile start
 ./createdb pagila
-./psql -d pagila -f ../../pagila/pagila-schema.sql
-./psql -d pagila -f ../../pagila/pagila-data.sql
-./psql -d pagila -f ../../pagila/pagila-insert-data.sql
+echo "Creating Toy Database ..."
+./psql -d pagila -f ../../pagila/pagila-schema.sql >/dev/null 2>&1
+./psql -d pagila -f ../../pagila/pagila-data.sql >/dev/null 2>&1
+./psql -d pagila -f ../../pagila/pagila-insert-data.sql >/dev/null 2>&1
+echo "Done!"
